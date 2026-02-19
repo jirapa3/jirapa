c82bfd72b786bb96da946c3f8ed1478cfbc732e1
jirapa <66010914002@msu.ac.th>
2026-02-20 04:23:45 +0700

    co

--- e/config.php
+++ e/config.php
@@ -1,11 +1,9 @@
 <?php
-session_start();
-
-$conn = new mysqli("localhost", "root", "", "4002db");
+$conn = new mysqli("localhost", "root", "รหัสผ่านจริงของเซิร์ฟเวอร์", "4002db");
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-mysqli_set_charset($conn, "utf8mb4");
+$conn->set_charset("utf8mb4");
 ?>
