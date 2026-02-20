f34271a669ceea02eec29b6edcb120ae7a6bf761
jirapa <66010914002@msu.ac.th>
2026-02-20 17:25:16 +0700

    c

--- h/connectdb.php
+++ h/connectdb.php
@@ -1,7 +1,26 @@
 <?php
-$conn = mysqli_connect("localhost","root","","4002db");
+include("connectdb.php");
 
-if (!$conn) {
-    die("เชื่อมต่อฐานข้อมูลไม่ได้");
+if(isset($_POST['submit'])){
+
+    $name = $_POST['name'];
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $sql = "INSERT INTO admin (a_name, a_username, a_password)
+            VALUES ('$name','$username','$password')";
+
+    mysqli_query($conn,$sql);
+
+    echo "บันทึกข้อมูลเรียบร้อย";
 }
 ?>
+
+<h1>เข้าสู่ระบบหลังบ้าน-จิราภา</h1>
+
+<form method="post">
+    ชื่อ <input type="text" name="name"><br><br>
+    Username <input type="text" name="username"><br><br>
+    Password <input type="password" name="password"><br><br>
+    <input type="submit" name="submit" value="LOGIN">
+</form>
